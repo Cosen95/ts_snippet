0cd0750600826738269c54b84c035c757fd32ec7
fengshuan <1263215592@qq.com>
2019-05-21 22:58:12 +0800

    feat:返回值类型 && 函数重载

--- src/example/inference-compatibility.ts
+++ src/example/inference-compatibility.ts
@@ -12,8 +12,8 @@ window.onmousedown = function(mouseEvent) {
 // x = y;
 
 // 参数类型
-let x = (a: number) => 0
-let y = (b: string) => 0
+// let x = (a: number) => 0
+// let y = (b: string) => 0
 // x = y
 
 // 函数参数双向协变
@@ -21,3 +21,25 @@ let funcA = (arg: number | string): void => {}
 let funcB = (arg: number): void => {}
 // funcA = funcB;
 funcB = funcA
+
+// 返回值类型
+let x = (): string | number => 0;
+let y = (): string => 'a'
+let z = (): boolean => false
+x = y
+// y = x
+// y = z
+
+// 函数重载
+function merge(arg1: number, arg2: number): number
+function merge(arg1: string, arg2: string): string
+function merge(arg1: any, arg2: any) {
+    return arg1 + arg2
+}
+
+function sum(arg1: number, arg2: number): number
+function sum(arg1: any, arg2: any): any {
+    return arg1 + arg2
+}
+let func = merge;
+// func = sum
