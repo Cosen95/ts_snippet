5b2b4896c8f46066d6673847ee9889bfd4802e40
fengshuan <1263215592@qq.com>
2019-05-23 21:41:54 +0800

    feat:高级类型

--- src/example/advanced-type.ts
+++ src/example/advanced-type.ts
@@ -14,3 +14,36 @@ const getLengthFunc = (content: string | number): number => {
         return content.toString().length
     }
 }
+
+
+// 类型保护
+// 1. paramName is Type
+// 2. typeof类型保护
+// 3. instanceof类型保护
+
+// 可辨识联合（Discriminated Unions）
+
+// 1.具有普通的单例类型属性
+// 2.一个类型别名包含了哪些类型的联合
+interface Square {
+    kind: 'square',
+    size: number
+}
+interface Rectangle {
+    kind: 'rectangle'
+    width: number
+    height: number
+}
+interface Circle {
+    kind: 'circle'
+    radius: number
+}
+
+type Shape = Square | Rectangle | Circle
+function area(s: Shape) {
+    switch(s.kind) {
+        case "square": return s.size * s.size;
+        case "rectangle": return s.width * s.height;
+        case "circle": return Math.PI * s.radius ** 2;
+    }
+}
