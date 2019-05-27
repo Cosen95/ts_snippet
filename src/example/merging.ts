8784a0c673e97684d93cb7c4cadf6f88a7f2bdd6
fengshuan <1263215592@qq.com>
2019-05-27 22:19:22 +0800

    feat:声明合并

---
+++ src/example/merging.ts
@@ -0,0 +1,41 @@
+import { namespace } from '../index';
+// 声明合并
+
+// 合并接口
+interface InfoInterface {
+    name: string
+}
+interface InfoInterface {
+    age: number
+}
+let infoInter: InfoInterface = {
+    name: 'fengshuan',
+    age: 21
+}
+
+// 合并命名空间和类
+class Validations {
+    constructor() {}
+    public checkType() {}
+}
+namespace Validations {
+    export const numberReg = /^[0-9]+$/
+}
+
+// 合并函数和命名空间
+function countUp() {
+    countUp.count++
+}
+namespace countUp {
+    export let count = 0
+}
+
+// 合并枚举和命名空间
+enum Colors {
+    red,
+    green,
+    blue
+}
+namespace Colors {
+    export const yellow = 3
+}
