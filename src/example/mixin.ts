b0c4447b26c1f76c14b543238f0adf6b7787de6d
fengshuan <1263215592@qq.com>
2019-05-29 22:05:01 +0800

    feat:minins混入

---
+++ src/example/mixin.ts
@@ -0,0 +1,36 @@
+// mixin 混入
+class Disposable {
+    isDisposed: boolean;
+    dispose() {
+        
+    }
+}
+class Activatable {
+    isActive: boolean;
+    activate() {
+
+    }
+    deactivate() {
+        
+    }
+}
+class SmartObject implements Disposable, Activatable {
+    isDisposed: boolean = false;
+    isActive: boolean = false;
+    dispose: () => void;
+    activate: () => void
+    deactivate: () => void
+    constructor() {
+        
+    }
+}
+function mixins(target: any, from: any[]) {
+    from.forEach((fromItem) => {
+        Object.getOwnPropertyNames(fromItem.prototype).forEach((key) => {
+            target.prototype[key] = fromItem.prototype[key]
+        })
+    })
+}
+mixins(SmartObject, [Disposable, Activatable])
+const smart = new SmartObject()
+console.log(smart)
